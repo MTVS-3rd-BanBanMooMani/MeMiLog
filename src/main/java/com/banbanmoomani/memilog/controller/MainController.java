4e3a12e1257535a83d5feb1ad3b309889ee72af3
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-07-14 13:37:40 +0900

    mydiary 페이지 타임리프 적용

--- src/main/java/com/banbanmoomani/memilog/controller/MainController.java
+++ src/main/java/com/banbanmoomani/memilog/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.banbanmoomani.memilog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
+    @GetMapping("/mydiary")
+    public void mydiary() {}
 }
