f517126a6ecd3c8c43125e8cfad159d4e87b452d
INUK-ai <arheon2469@gmail.com>
2024-07-15 18:11:14 +0900

    Feat: AdminService - getBlackListDTO

--- src/main/java/com/banbanmoomani/memilog/service/AdminService.java
+++ src/main/java/com/banbanmoomani/memilog/service/AdminService.java
@@ -2,6 +2,11 @@ package com.banbanmoomani.memilog.service;
 
 import com.banbanmoomani.memilog.DAO.AdminDAO;
 import com.banbanmoomani.memilog.DTO.AdminDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.io.IOException;
+
+import static com.banbanmoomani.memilog.config.Template.getSqlSession;
 
 public class AdminService {
 
@@ -12,6 +17,18 @@ public class AdminService {
     }
 
     public AdminDTO.getBlackListDTO getBlackListDTO() {
+
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = getSqlSession();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        AdminDTO.getBlackListDTO getBlackListDTO = adminDAO.getBlackList();
+
+        sqlSession.close();
+
         return null;
     }
 }
