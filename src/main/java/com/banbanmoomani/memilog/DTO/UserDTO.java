ed5e0d11e4ae4f1167face55daa6c907ad3599b5
in seong Park <74069492+Hexeong@users.noreply.github.com>
2024-07-17 16:33:29 +0900

    - refactor domain:user DTO class - feature Signup

--- src/main/java/com/banbanmoomani/memilog/DTO/UserDTO.java
+++ src/main/java/com/banbanmoomani/memilog/DTO/UserDTO.java
@@ -1,5 +1,138 @@
 package com.banbanmoomani.memilog.DTO;
 
-public class UserDTO {
+import java.sql.Date;
 
+public class UserDTO {
+    private int user_id;
+    private String email;
+    private String password;
+    private java.sql.Date birthday;
+    private String nickName;
+    private String gender;
+    private int caution_weights;
+    private String temporary_YN;
+    private String today_access_YN;
+    private java.sql.Date signup_date;
+    private java.sql.Date susp_period;
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "user_id=" + user_id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", birthday=" + birthday +
+                ", nickName='" + nickName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", caution_weights=" + caution_weights +
+                ", temporary_YN='" + temporary_YN + '\'' +
+                ", today_access_YN='" + today_access_YN + '\'' +
+                ", signup_date=" + signup_date +
+                ", susp_period=" + susp_period +
+                '}';
+    }
+
+    public UserDTO() {}
+
+    public UserDTO(int user_id, String email, String password, Date birthday, String nickName, String gender, int caution_weights, String temporary_YN, String today_access_YN, Date signup_date, Date susp_period) {
+        this.user_id = user_id;
+        this.email = email;
+        this.password = password;
+        this.birthday = birthday;
+        this.nickName = nickName;
+        this.gender = gender;
+        this.caution_weights = caution_weights;
+        this.temporary_YN = temporary_YN;
+        this.today_access_YN = today_access_YN;
+        this.signup_date = signup_date;
+        this.susp_period = susp_period;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getCaution_weights() {
+        return caution_weights;
+    }
+
+    public void setCaution_weights(int caution_weights) {
+        this.caution_weights = caution_weights;
+    }
+
+    public String getTemporary_YN() {
+        return temporary_YN;
+    }
+
+    public void setTemporary_YN(String temporary_YN) {
+        this.temporary_YN = temporary_YN;
+    }
+
+    public String getToday_access_YN() {
+        return today_access_YN;
+    }
+
+    public void setToday_access_YN(String today_access_YN) {
+        this.today_access_YN = today_access_YN;
+    }
+
+    public Date getSignup_date() {
+        return signup_date;
+    }
+
+    public void setSignup_date(Date signup_date) {
+        this.signup_date = signup_date;
+    }
+
+    public Date getSusp_period() {
+        return susp_period;
+    }
+
+    public void setSusp_period(Date susp_period) {
+        this.susp_period = susp_period;
+    }
 }
--- src/main/java/com/banbanmoomani/memilog/controller/UserController.java
+++ src/main/java/com/banbanmoomani/memilog/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.banbanmoomani.memilog.controller;
 
-import com.banbanmoomani.memilog.DTO.UserDTO;
+import com.banbanmoomani.memilog.DTO.SignUpRequestDTO;
+import com.banbanmoomani.memilog.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/login")
     public String login() {
@@ -22,15 +29,19 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(UserDTO userDTO) {
-        // service : 해당 이메일 중복인지 확인 후 DAO의 insertUser 호출
+    public String signup(SignUpRequestDTO signUpRequestDTO, RedirectAttributes rttr, Model model) {
+        // 중복 확인
+        boolean duplicateCheck = userService.duplicateCheck(signUpRequestDTO);
+        if(duplicateCheck) {
+            // 중복 시 alert창 띄우면서 signup페이지로 되돌아가기
+            rttr.addFlashAttribute("failMessage", "닉네임이 중복되었습니다. 다시 회원가입해주세요");
+            return "redirect:/signup";
+        }
 
-        // DAO : MyBatis의 insertUser 호출
 
-        // service에서 false 반환시 alert 띄우기
-
-        // true 반환시 index page로 이동
-        return "main/home";
+        userService.insertUser(signUpRequestDTO);
+        rttr.addFlashAttribute("successMessage", "회원가입 되셨습니다. "+signUpRequestDTO.getNickname()+"환영합니다");
+        return "redirect:/";
     }
 
     @GetMapping("/forgot")
