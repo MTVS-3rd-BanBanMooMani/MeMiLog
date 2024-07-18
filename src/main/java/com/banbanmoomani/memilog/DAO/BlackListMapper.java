6a99dca321cc8e2f8b202d76208d8e124cb41f2d
INUK-ai <arheon2469@gmail.com>
2024-07-18 09:17:16 +0900

    Feat: BlackListMapper - getBlackList();

--- src/main/java/com/banbanmoomani/memilog/DAO/BlackListMapper.java
+++ src/main/java/com/banbanmoomani/memilog/DAO/BlackListMapper.java
@@ -1,6 +1,7 @@
 package com.banbanmoomani.memilog.DAO;
 
 import com.banbanmoomani.memilog.DTO.admin.blacklist.BanListDTO;
+import com.banbanmoomani.memilog.DTO.admin.blacklist.BlackListDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @Mapper
 public interface BlackListMapper {
 
-    // TODO: perm_YN = 'N' 인 STOP 과 USER 를 JOIN 해서 데이터 수정 및 가져옴
     List<BanListDTO> getBanList();
+
+    List<BlackListDTO> getBlackList();
 }
