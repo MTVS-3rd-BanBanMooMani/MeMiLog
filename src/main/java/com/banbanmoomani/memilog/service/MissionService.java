0920edd49918331e4642eff6540daa2311c77b4a
hojin0729 <0918syj@gmail.com>
2024-07-20 19:12:48 +0900

    Feat: mission 타임리프

--- src/main/java/com/banbanmoomani/memilog/service/MissionService.java
+++ src/main/java/com/banbanmoomani/memilog/service/MissionService.java
@@ -4,6 +4,8 @@ import com.banbanmoomani.memilog.DAO.MissionMapper;
 import com.banbanmoomani.memilog.DTO.MissionDTO;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,6 +17,10 @@ public class MissionService {
         this.missionMapper = missionMapper;
     }
 
+    public MissionDTO findMissionByContent(String content) {
+        return missionMapper.findMissionByContent(content);
+    }
+
     public List<MissionDTO> findAllMission() {
         return missionMapper.findAllMission();
     }
@@ -24,4 +30,26 @@ public class MissionService {
     }
 
     public List<MissionDTO> missionDetailByDate() { return missionMapper.missionDetailByDate(); }
+
+    public MissionDTO findTodayMission() {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String today = dateFormat.format(new Date());
+        System.out.println(today);
+        MissionDTO missionDTO = missionMapper.findTodayMission(today);
+
+        System.out.println("service : " + missionDTO);
+        return missionMapper.findTodayMission(today);
+    }
+
+    public String findPriThemeName(int priThemeId) {
+        return missionMapper.findPriThemeName(priThemeId);
+    }
+    public String findSubThemeName(int subThemeId) {
+        return missionMapper.findSubThemeName(subThemeId);
+    }
+    public MissionDTO findMissionById(int missionId) {
+        return missionMapper.findMissionById(missionId);
+    }
+
 }
