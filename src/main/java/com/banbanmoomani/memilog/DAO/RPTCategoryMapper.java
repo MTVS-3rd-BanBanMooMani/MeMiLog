664ec4d0ff8a79f5d1778a41f0dd1709f30d2225
mataweed <wngus12op@naver.com>
2024-07-21 18:03:10 +0900

    Feat: RPTCategory -BanPoint add new reason 구현

--- src/main/java/com/banbanmoomani/memilog/DAO/RPTCategoryMapper.java
+++ src/main/java/com/banbanmoomani/memilog/DAO/RPTCategoryMapper.java
@@ -1,10 +1,12 @@
 package com.banbanmoomani.memilog.DAO;
 
 import com.banbanmoomani.memilog.DTO.admin.report.RPTCategoryDTO;
+import com.banbanmoomani.memilog.DTO.admin.report.RPTCategoryRequestDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 @Mapper
 public interface RPTCategoryMapper {
     List<RPTCategoryDTO> findAllRPTCategory();
+    void createRPTCategory(RPTCategoryRequestDTO rptCategoryRequestDTO);
 }
--- src/main/java/com/banbanmoomani/memilog/service/RPTCategoryService.java
+++ src/main/java/com/banbanmoomani/memilog/service/RPTCategoryService.java
@@ -2,6 +2,7 @@ package com.banbanmoomani.memilog.service;
 
 import com.banbanmoomani.memilog.DAO.RPTCategoryMapper;
 import com.banbanmoomani.memilog.DTO.admin.report.RPTCategoryDTO;
+import com.banbanmoomani.memilog.DTO.admin.report.RPTCategoryRequestDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,10 @@ public class RPTCategoryService {
 
     public List<RPTCategoryDTO> findAllCategorise() {
         return rptCategoryMapper.findAllRPTCategory();
+    }
+
+    public void createRPTCategory(RPTCategoryRequestDTO rptCategoryRequestDTO) {
+        rptCategoryMapper.createRPTCategory(rptCategoryRequestDTO);
     }
 
 
