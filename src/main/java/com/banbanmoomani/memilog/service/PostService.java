03216df8abfef8b9acb5d3d249770ae894534477
INUK-ai <arheon2469@gmail.com>
2024-07-24 12:45:18 +0900

    Refactor: 최대 이미지 크기 확대 설정

--- src/main/java/com/banbanmoomani/memilog/service/PostService.java
+++ src/main/java/com/banbanmoomani/memilog/service/PostService.java
@@ -11,6 +11,7 @@ import com.banbanmoomani.memilog.DTO.todayPostDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -42,15 +43,20 @@ public class PostService {
 //        }
 //    }
     @Transactional
-    public void updatePost(PostDTO updateRequestDTO) {
-    // 하드코딩된 post_id를 사용하여 게시물을 찾습니다.
-        int postId = 35;
-        int userId = 1;
+    public void updatePost(PostDTO updateRequestDTO, int userId) {
+
+        int postId = updateRequestDTO.getPost_id();
+
         PostDTO post = postMapper.findPostById(postId);
         System.out.println("Post: " + post);
         System.out.println("UpdateRequest User ID: " + userId);
 
-        if (post != null && post.getUser_id() == userId) {
+        LocalDate localDate = LocalDate.from(post.getWritten_datetime().toLocalDateTime());
+        if(!localDate.equals(LocalDate.now())) {
+            throw new IllegalArgumentException("The post date is not today.");
+        }
+
+        if (post.getUser_id() == userId) {
         // 하드코딩된 post_id와 user_id를 설정
             updateRequestDTO.setPost_id(postId);
             updateRequestDTO.setUser_id(userId);
