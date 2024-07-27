2022e23bd6ae0cd048353b78de7b295540606490
INUK-ai <arheon2469@gmail.com>
2024-07-27 10:38:28 +0900

    Refactor: Main 페이지에서의 검색

--- src/main/resources/static/document/js/missionPost.js
+++ src/main/resources/static/document/js/missionPost.js
@@ -1,12 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const params = new URLSearchParams(window.location.search);
+  const word = params.get('word');
 
-// document.addEventListener("DOMContentLoaded", function () {
-  // 테마 카테고리
+  if (word) {
+    fetch(`/theme?word=${word}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log("검색 mission: ", data);
+          notesData = data; // notesData를 업데이트
+          renderNotes(1);
+          renderPagination();
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  } else {
+    // 메모지 데이터 받아오기
+    fetch("/main/allMission")
+        .then(res => res.json()) // 응답을 JSON 형식으로 변환
+        .then(data => {
+          console.log("Fetched data:", data);
+          notesData = data;
+          renderNotes(1);
+          renderPagination();
+        })
+        .catch(error => console.error("Error fetching data:", error));
+  }
+
   const categoryBtns = document.querySelectorAll(".category-btn");
-
   const selectedCategories = [];
 
   function addValue(button, value) {
-    if(!selectedCategories.includes(value)) {
+    if (!selectedCategories.includes(value)) {
       const inputTag = document.getElementById('themeType');
       selectedCategories.push(value);
       inputTag.value = selectedCategories.toString();
@@ -23,7 +46,7 @@
     const cancelBtn = document.createElement("span");
     cancelBtn.classList.add("cancel-btn");
     cancelBtn.innerHTML = "&#10006;";
-    cancelBtn.onclick = function (event) {
+    cancelBtn.onclick = function(event) {
       event.stopPropagation();
       removeValue(button);
     };
@@ -32,7 +55,7 @@
 
   function removeCancelButton(button) {
     const cancelBtn = button.querySelector('.cancel-btn');
-    if(cancelBtn) {
+    if (cancelBtn) {
       button.removeChild(cancelBtn);
     }
   }
@@ -79,11 +102,11 @@
     }
   }
 
-// 폼 제출 이벤트 리스너 추가
-document.getElementById('themeWordForm').addEventListener('submit', (event) => {
-  event.preventDefault(); // 기본 폼 제출 동작을 막음
-  submitForm(); // 비동기 요청을 보내는 submitForm 호출
-});
+  // 폼 제출 이벤트 리스너 추가
+  document.getElementById('themeWordForm').addEventListener('submit', (event) => {
+    event.preventDefault(); // 기본 폼 제출 동작을 막음
+    submitForm(); // 비동기 요청을 보내는 submitForm 호출
+  });
 
   document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
@@ -97,11 +120,8 @@ document.getElementById('themeWordForm').addEventListener('submit', (event) => {
         }
       })
     }
-  })
+  });
 
-  // ==================================================================================
-  // mission note(메모지) 관련
-  // 메모지와 마스킹테이프 색깔
   const noteColors = [
     "#fffafa", "#F6FDFF", "#FFFFF5", "#FBF9FF", "#F3FFFD", "#FFF7FC", "#F7FFF3", "#FFF8F2",
   ];
@@ -109,52 +129,29 @@ document.getElementById('themeWordForm').addEventListener('submit', (event) => {
     "#FBD5D5", "#D5F4FB", "#FBFAD5", "#EDD5FB", "#D5FBF4", "#FBD5EE", "#E1FBD5", "#FFC692",
   ];
 
-// 메모지 div 추가할 요소
   const noteContainer = document.getElementById("note-container");
-// 페이지 버튼 표시할 요소
   const pagination = document.getElementById("pagination");
 
-// 메모지 데이터 저장할 배열
   let notesData = [];
-// 한 페이지에 표시할 메모지의 개수
   const notesPerPage = 24;
 
-// 메모지 데이터 받아오기
-  fetch("/main/allMission")
-      .then(res => res.json()) // 응답을 JSON 형식으로 변환
-      // 데이터를 성공적으로 받았을 때,
-      .then(data => {
-        console.log("Fetched data:", data);
-        notesData = data;
-        renderNotes(1);
-        renderPagination();
-      })
-      .catch(error => console.error("Error fetching data:", error));
-
-  // 메모지(노트) 렌더링 함수
-  // 주어진 페이지 번호에 해당하는 메모지들을 화면에 렌더링
   function renderNotes(page) {
     noteContainer.innerHTML = "";
     const start = (page - 1) * notesPerPage;
     const end = start + notesPerPage;
     const notesToDisplay = notesData.slice(start, end);
 
-    // 각 메모지들을 화면에 렌더링 함
     notesToDisplay.forEach(note => {
-      // console.log("Rendering note:", note);
-
-      // 날짜 변환
       const [year, month, day] = note.missionDate.split('-');
       const formattedDate = `FROM.${year.slice(2)}${month}${day}`;
 
       const noteElement = document.createElement("div");
       noteElement.className = "note";
       noteElement.innerHTML = `
-            <div class="masking-tape"></div>
-            <span>${formattedDate}</span>
-            <span>${note.missionContent}</span>
-        `;
-      // 메모지 무작위 색상
+                <div class="masking-tape"></div>
+                <span>${formattedDate}</span>
+                <span>${note.missionContent}</span>
+            `;
       const randomIndex = Math.floor(Math.random() * noteColors.length);
       const selectedNoteColor = noteColors[randomIndex];
       const selectedTapeColor = tapeColors[randomIndex];
@@ -162,29 +159,22 @@ document.getElementById('themeWordForm').addEventListener('submit', (event) => {
       noteElement.style.backgroundColor = selectedNoteColor;
       noteElement.querySelector(".masking-tape").style.backgroundColor = selectedTapeColor;
       noteElement.addEventListener("click", () => {
-        // location.href = "/allmission?post_id="+note["missionId"]
-        location.href = "/post/bymission?date="+note["missionDate"]
-      })
+        location.href = `/post/bymission?date=${note.missionDate}`;
+      });
       noteContainer.appendChild(noteElement);
-
     });
   }
 
-// ==================================================================================
-
-// 페이지네이션 렌더링 함수
   function renderPagination() {
     pagination.innerHTML = "";
-    // 필요한 총 페이지 수를 계산함
     const pageCount = Math.ceil(notesData.length / notesPerPage);
 
-    // 각 페이지에 대한 버튼 생성
     for (let i = 1; i <= pageCount; i++) {
       const pageBtn = document.createElement("div");
       pageBtn.className = "page-btn";
       pageBtn.textContent = i;
       if (i === 1) pageBtn.classList.add("active");
-      pageBtn.addEventListener("click", function () {
+      pageBtn.addEventListener("click", function() {
         document.querySelectorAll(".page-btn").forEach((btn) => {
           btn.classList.remove("active");
         });
@@ -194,27 +184,4 @@ document.getElementById('themeWordForm').addEventListener('submit', (event) => {
       pagination.appendChild(pageBtn);
     }
   }
-
-  renderNotes(1);
-  renderPagination();
-// });
-
-// function likeFetch() {
-//   // const postIdValue = document.getElementById('id').value; 나중에 여기에 값 넣어서 주석 제거하고 사용하시면 됩니다.
-//   const postIdValue = 49
-//   fetch("/post/like?post_id="+postIdValue)
-//       .then(res => res.json())
-//       .then(data => {
-//         console.log(data);
-//       })
-// }
-//
-// function dislikeFetch() {
-//   // const postIdValue = document.getElementById('id').value; 나중에 여기에 값 넣어서 주석 제거하고 사용하시면 됩니다.
-//   const postIdValue = 49
-//   fetch("/post/dislike?post_id="+postIdValue)
-//       .then(res => res.json())
-//       .then(data => {
-//         console.log(data);
-//       })
-// }
+});
