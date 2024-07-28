10bdf6fa33194f46bd7bb88c885c5f4a5c529a86
INUK-ai <arheon2469@gmail.com>
2024-07-28 21:00:54 +0900

    Refactor: 필요없는 거 삭제

--- src/main/java/com/banbanmoomani/memilog/config/error/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.banbanmoomani.memilog.config.error;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/banbanmoomani/memilog/config/error/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.banbanmoomani.memilog.config.error;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
